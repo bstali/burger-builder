56f2f85598e7a5689d8e2547025a80963542482d
bstali <bstali56@gmail.com>
2021-04-19 16:50:05 +0500

    Adding More Features

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import Aux from '../Aux/Aux';
+import classes from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component  {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawertoggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render (){
+        return(
+            <Aux >
+    <Toolbar drawToggleClicked={this.sideDrawertoggleHandler}/>
+    <SideDrawer 
+    closed={this.sideDrawerClosedHandler}
+    open={this.state.showSideDrawer}/>
+    <main className={classes.Content}>
+        {this.props.children}
+    </main>
+   </Aux> 
+        );
+    } }
+
+export default Layout;
